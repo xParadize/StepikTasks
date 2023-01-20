037e2f99709a3f3d65afe649d753dd7bfb4ba12c
xParadize <73064983+xParadize@users.noreply.github.com>
2023-01-20 12:46:12 +0300

    Update Stepik.java

--- Stepik.java
+++ Stepik.java
@@ -1,13 +1,34 @@
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.Scanner;
 
 public class Stepik {
-
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
         File file = new File("input.txt");
-        Scanner sc = new Scanner(file);
-        String s = sc.nextLine();
-        System.out.println(s);
+        Scanner sc1 = new Scanner(file);
+        Scanner sc2 = new Scanner(file);
+        FileWriter fw = new FileWriter("output.txt");
+        while (sc1.hasNext()) {
+            try {
+                double a = sc1.nextDouble();
+                String op = sc1.next();
+                double b = sc1.nextDouble();
+                String sign = "+-*/";
+                String result = "";
+                if (!sign.contains(op)) result = "Operation Error!";
+                switch (op) {
+                    case "+": result = Double.toString(a + b); break;
+                    case "-": result = Double.toString(a - b); break;
+                    case "*": result = Double.toString(a * b); break;
+                    case "/": result = (b == 0 ? "Error! Division by zero" : Double.toString(a / b)); break;
+                }
+                fw.append(sc2.nextLine() + " = " + result + "\n");
+                fw.flush();
+            } catch (Exception ex) {
+                fw.append(sc2.nextLine() + " = " + "Error! Not number" + "\n");
+                fw.flush();
+                sc1.nextLine();
+            }
+        }
+        fw.close(); sc1.close(); sc2.close();
     }
 }
